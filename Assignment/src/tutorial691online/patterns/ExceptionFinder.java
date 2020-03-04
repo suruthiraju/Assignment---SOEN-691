18914da5542050876d9347a7c81bbced2f6392cf
Nasim Adabi <nasim.adabi@gmail.com>
2020-03-03 19:28:50 -0500

    First anti pattern detector edited and Open source project to test first anti pattern detection added

--- Assignment/src/tutorial691online/patterns/ExceptionFinder.java
+++ Assignment/src/tutorial691online/patterns/ExceptionFinder.java
@@ -13,40 +13,42 @@ import org.eclipse.jdt.core.dom.*;
 
 public class ExceptionFinder {
 	HashMap<MethodDeclaration, String> suspectMethods = new HashMap<>();
-	
+	HashMap<MethodDeclaration, String> throwMethods = new HashMap<>();
+
 	public void findExceptions(IProject project) throws JavaModelException {
 		IPackageFragment[] packages = JavaCore.create(project).getPackageFragments();
-		for(IPackageFragment mypackage : packages){
+		
+		for (IPackageFragment mypackage : packages) {
+			
 			for (ICompilationUnit unit : mypackage.getCompilationUnits()) {
-				//AST node
+				// AST node
 				CompilationUnit parsedCompilationUnit = parse(unit);
 				
-				//do method visit here and check stuff
+				// do method visit here and check stuff
 				CatchClauseVisitor exceptionVisitor = new CatchClauseVisitor();
 				parsedCompilationUnit.accept(exceptionVisitor);
 
-				//Give detail of detection
+				// Give detail of detection
 				getMethodsWithTargetCatchClauses(exceptionVisitor);
 			}
 		}
 	}
 
 	private void getMethodsWithTargetCatchClauses(CatchClauseVisitor catchClauseVisitor) {
-		
-		for(CatchClause emptyCatch: catchClauseVisitor.getEmptyCatches()) {
-			suspectMethods.put(findMethodForCatch(emptyCatch), "EmptyCatch");
-		}	
-		
-		for(CatchClause dummyCatch: catchClauseVisitor.getDummyCatches()) {
-			suspectMethods.put(findMethodForCatch(dummyCatch), "DummyCatch");
-		}
+//		for(CatchClause emptyCatch: catchClauseVisitor.getEmptyCatches()) {
+//			suspectMethods.put(findMethodForCatch(emptyCatch), "EmptyCatch");
+//		}	
+//		
+//		for(CatchClause dummyCatch: catchClauseVisitor.getDummyCatches()) {
+//			suspectMethods.put(findMethodForCatch(dummyCatch), "DummyCatch");
+//		}
 		
 		for(CatchClause throwStatement: catchClauseVisitor.getThrowStatements()) {
 			suspectMethods.put(findMethodForThrow(throwStatement), "throwStatement");
+			throwMethods.put(findMethodForCatch(throwStatement), "LogThrow");
 		}
-		
 	}
-	
+
 	private ASTNode findParentMethodDeclaration(ASTNode node) {
 		if(node.getParent().getNodeType() == ASTNode.METHOD_DECLARATION) {
 			return node.getParent();
@@ -66,15 +68,16 @@ public class ExceptionFinder {
 	public HashMap<MethodDeclaration, String> getSuspectMethods() {
 		return suspectMethods;
 	}
-	
+
 	public void printExceptions() {
-		for(MethodDeclaration declaration : suspectMethods.keySet()) {
+		for (MethodDeclaration declaration : suspectMethods.keySet()) {
 			String type = suspectMethods.get(declaration);
-			SampleHandler.printMessage(String.format("The following method suffers from the %s pattern", type));
+			SampleHandler.printMessage(String.format("The following method suffers from the %s anti-pattern", type));
 			SampleHandler.printMessage(declaration.toString());
 		}
+		SampleHandler.printMessage(String.format("Throw & Log anti-pattern Detected Count: %s", throwMethods.size()));
 	}
-	
+
 	public static CompilationUnit parse(ICompilationUnit unit) {
 		@SuppressWarnings("deprecation")
 		ASTParser parser = ASTParser.newParser(AST.JLS8);
